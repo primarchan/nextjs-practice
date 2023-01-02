6245bb1aaceb48f0398ebd6e6ba2f59044b22076
An T.G <boltcarrier@naver.com>
2023-01-03 04:54:58 +0900

    catch all

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,7 @@ import Seo from "../components/Seo";
 export default function Home({ results }) {
     const router = useRouter();
     const onClick = (id, title) => {
-        router.push(
-            {
-                pathname: `/movies/${id}`,
-                query: {
-                    title,
-                },
-            },
-            `/movies/${id}`
-        );
+        router.push(`/movies/${title}/${id}`);
     }
     return (
         <div className="container">
@@ -22,14 +14,7 @@ export default function Home({ results }) {
             {results?.map((movie) => (
                 <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
                     <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-                    <Link href={{
-                        pathname: `/movies/${movie.id}`,
-                        query: {
-                            title: movie.original_title,
-                        },
-                    }}
-                    as={`/movies/${movie.id}`}
-                    legacyBehavior>
+                    <Link href={`/movies/${movie.original_title}/${movie.id}`} legacyBehavior>
                         <a><h4>{movie.original_title}</h4></a>
                     </Link>
                 </div>
